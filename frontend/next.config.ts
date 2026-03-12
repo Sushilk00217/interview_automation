c8306b90fff4bd8c186fdaf5484174c019a8497c
Sushilk00217 <sushil.8.kumar@coforge.com>
2026-03-12 17:06:48 +0530

    Configure images for static hosting
    Set images to unoptimized for static hosting.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,15 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  // Recommended for Static Hosting (avoids next/image optimizer needing a server)
   images: {
     unoptimized: true,
   },
 };
+
+export default nextConfig;
+
 
